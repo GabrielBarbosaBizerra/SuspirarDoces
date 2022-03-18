0fc5ed414998bb58883354052b5bdf56dd34f4ca
Gabriel Barbosa Bizerra <gabrielbarbosabizerra@gmail.com>
2022-03-18 19:29:21 -0300

    Criado o serviço de configuração do AutoMapper

---
+++ SuspirarDoces.API/ConfigurationMapping/AutoMapperConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SuspirarDoces.Application.Mappings;
+using System;
+
+namespace SuspirarDoces.API.ConfigurationMapping
+{
+    public static class AutoMapperConfiguration
+    {
+        public static void AddAutoMapperConfiguration(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddAutoMapper(typeof(DomainToViewModel),
+                typeof(ViewModelToDomain));
+        }
+    }
+}
