31eee224ecc184734776d42d825d5f982cd9751f
Gabriel Barbosa Bizerra <gabrielbarbosabizerra@gmail.com>
2022-03-18 19:25:17 -0300

    Adicionado inicialmente os services

--- SuspirarDoces.Infrastructure.IoC/DependencyInjection.cs
+++ SuspirarDoces.Infrastructure.IoC/DependencyInjection.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SuspirarDoces.Application.Interfaces;
+using SuspirarDoces.Application.Services;
+using SuspirarDoces.Application.ViewsModel;
+using SuspirarDoces.Domain.Entities;
+using SuspirarDoces.Domain.Interfaces;
 using SuspirarDoces.Infrastructure.Data.Context;
+using SuspirarDoces.Infrastructure.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +22,27 @@ namespace SuspirarDoces.Infrastructure.IoC
         {
             services.AddEntityFrameworkNpgsql().AddDbContext<DataContext>(options =>
                 options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IRepository<Cliente>, ClientRepository>();
+            services.AddScoped<IRepository<Entrada>, FinancialEntryRepository>();
+            services.AddScoped<IRepository<Saida>, FinancialOutputRepository>();
+            services.AddScoped<IRepository<Resultado>, FinancialResultRepository>();
+            services.AddScoped<IRepository<Ingrediente>, IngredientRepository>();
+            services.AddScoped<IRepository<Estoque>, InventoryRepository>();
+            services.AddScoped<IRepository<Pedido>, OrderRepository>();
+            services.AddScoped<IRepository<ProdutoPedido>, OrderedProduct>();
+            services.AddScoped<IRepository<Produto>, ProductRepository>();
+            services.AddScoped<IRepository<Usuario>, UserRepository>();
+
+            services.AddScoped<IService<ClientViewModel>, ClientService>();
+            services.AddScoped<IService<FinancialEntryViewModel>, FinancialEntryService>();
+            services.AddScoped<IService<FinancialOutputViewModel>, FinancialOutputService>();
+            services.AddScoped<IService<FinancialResultViewModel>, FinancialResultService>();
+            services.AddScoped<IService<IngredientViewModel>, IngredientService>();
+            services.AddScoped<IService<InventoryViewModel>, InventoryService>();
+            services.AddScoped<IService<OrderViewModel>, OrderService>();
+            services.AddScoped<IService<OrderedProductViewModel>, OrderedProductService>();
+            services.AddScoped<IService<ProductViewModel>, ProductService>();
+            services.AddScoped<IService<UserViewModel>,UserService>();
 
             return services;
         }
