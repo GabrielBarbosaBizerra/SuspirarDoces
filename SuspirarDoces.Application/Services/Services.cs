f02ae0f435b886cf17a15a5ab40488fc8d82901e
Gabriel Barbosa Bizerra <gabrielbarbosabizerra@gmail.com>
2022-03-21 15:58:12 -0300

    Criação inicial do serviço para encriptar senha

---
+++ SuspirarDoces.Application/Services/Services.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuspirarDoces.Application.Services
+{
+    public class Services
+    {
+        public static string EncriptarSenhas(string senha)
+        {
+            HashAlgorithm sha = new SHA1CryptoServiceProvider();
+            byte[] senhaEncriptada = sha.ComputeHash(Encoding.UTF8.GetBytes(senha));
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var caractere in senhaEncriptada)
+            {
+                stringBuilder.Append(caractere.ToString("X2"));
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
