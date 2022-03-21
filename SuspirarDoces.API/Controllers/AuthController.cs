9a02252b3e3f7509cc291f2f733996c0f2849875
Gabriel Barbosa Bizerra <gabrielbarbosabizerra@gmail.com>
2022-03-21 15:56:11 -0300

    Criação inicial do Controller de autenticação

---
+++ SuspirarDoces.API/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SuspirarDoces.Application.Interfaces;
+using SuspirarDoces.Application.ViewsModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuspirarDoces.API.Controllers
+{
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthenticateService _authService;
+        private readonly IConfiguration _configuration;
+        public AuthController(IAuthenticateService authService, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route("/autenticar")]
+        public ActionResult Authenticate(UserViewModel user)
+        {
+            if (string.IsNullOrEmpty(user.Email))
+            {
+                return BadRequest("Insira um email");
+            }
+            if (string.IsNullOrEmpty(user.Senha))
+            {
+                return BadRequest("Insira a senha");
+            }
+
+            try
+            {
+                string secretKey = _configuration["ChaveSecreta"];
+                var token = _authService.Authenticate(user, secretKey);
+                return StatusCode(StatusCodes.Status200OK, token);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+
+        }
+    }
+}
