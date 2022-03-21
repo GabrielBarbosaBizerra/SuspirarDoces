c3d8b0feb0476273e06ae48e608dda9c4967da47
Gabriel Barbosa Bizerra <gabrielbarbosabizerra@gmail.com>
2022-03-21 15:54:56 -0300

    Adição de ignoração de objetos cíclicos JTW

--- SuspirarDoces.API/Startup.cs
+++ SuspirarDoces.API/Startup.cs
@@ -28,6 +28,10 @@ namespace SuspirarDoces.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                                        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                                    );
             services.AddInfrastructure(Configuration);
             services.AddAutoMapperConfiguration();
             services.AddControllers();
