6c8b8ce9b59a363f71e9123f26de27f58313de45
Gabriel Barbosa Bizerra <gabrielbarbosabizerra@gmail.com>
2022-04-03 12:51:36 -0300

    Criação da ViewModel responsável por guardar as propriedades email e token de autenticação

---
+++ SuspirarDoces.Application/ViewsModel/UserTokenViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuspirarDoces.Application.ViewsModel
+{
+    public class UserTokenViewModel
+    {
+        public UserTokenViewModel(string email, string token)
+        {
+            Email = email;
+            Token = token;
+        }
+        public string Email { get; private set; }
+        public string Token { get; private set; }
+    }
+}
